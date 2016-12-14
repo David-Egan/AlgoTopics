71f5618e36265c3078650bcb26a44494881d496a
David Egan <davideganse@gmail.com>
2016-12-13 21:43:00 -0500

    Initial code commit

---
+++ src/DataStructures/LinkedList/LinkedList.java
@@ -0,0 +1,12 @@
+package DataStructures.LinkedList;
+
+/**
+ * Created by degan on 12/13/2016.
+ */
+public class LinkedList {
+    Node head;
+
+    public LinkedList(){
+
+    }
+}
---
+++ src/DataStructures/LinkedList/Node.java
@@ -0,0 +1,13 @@
+package DataStructures.LinkedList;
+
+/**
+ * Created by degan on 12/13/2016.
+ */
+public class Node {
+    Node next;
+    int data;
+
+    public Node(){
+    }
+
+}
